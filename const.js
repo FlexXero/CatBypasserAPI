747cde551731e0992eeb8265eea6baa7fffea6b4
Xero <124565253+FlexXero@users.noreply.github.com>
2024-06-30 00:48:03 +1200

    Create const.js

---
+++ const.js
@@ -0,0 +1,13 @@
+// const.js
+
+export const API_URLS = {
+    bypassi: 'https://bypassi.goatbypassers.xyz/?url=',
+    ethosbypass: 'https://et.goatbypassers.xyz/api/adlinks/bypass?url='
+};
+
+export const DEFAULT_API = API_URLS.bypassi; 
+
+export const API_CREDITS = {
+    ethosbypass: "Ethos API Made By Shehajeez.",
+    bypassi: "Bypassi API Made By wmnd."
+};
