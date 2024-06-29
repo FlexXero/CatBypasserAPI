75fe2e39f23220f9e8e43f5e11c92b5c5612e17a
Xero <124565253+FlexXero@users.noreply.github.com>
2024-06-30 00:46:53 +1200

    Create errors.js

---
+++ errors.js
@@ -0,0 +1,19 @@
+export const METHOD = {
+    success: false,
+    message: 'Please use a POST request to communicate with the API.'
+}
+
+
+export const NO_PARAMETERS = {
+    success: false,
+    message: 'Please enter the required parameters.'
+};
+
+
+export const NO_URL = {
+    success: false,
+    message: 'Please enter a URL as a parameter.'
+};
+
+
+export const NO_RESULT = 'The API result is empty.';
